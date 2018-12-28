8053a5a7e03a9516630be82ab0bd610aa3082d65
Vadim <testov@test.ua>
2018-12-28 17:55:17 +0200

    .

--- Course2Lesson5Task1/src/ua/testov/test/Main.java
+++ Course2Lesson5Task1/src/ua/testov/test/Main.java
@@ -3,13 +3,11 @@ package ua.testov.test;
 import java.io.File;
 import java.io.IOException;
 
-
-
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File in = new File("test.pdf");
+		File in = new File("test.docx");
 		File out = new File("copy.doc");
 		try {
 			FileOperation.fileCopyDocx(in, out);
