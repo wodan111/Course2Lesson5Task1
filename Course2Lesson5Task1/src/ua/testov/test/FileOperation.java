c86a4d5512798a8fa77c449f0325fcac46289bf4
Vadim <testov@test.ua>
2018-12-28 19:27:12 +0200

    .

--- Course2Lesson5Task1/src/ua/testov/test/FileOperation.java
+++ Course2Lesson5Task1/src/ua/testov/test/FileOperation.java
@@ -28,7 +28,7 @@ public class FileOperation {
 				throw e;
 			}
 		} else {
-			System.out.println("Файл повинен бути формату .doc, .docx");  
+			System.out.println("Р¤Р°Р№Р» РїРѕРІРёРЅРµРЅ Р±СѓС‚Рё С„РѕСЂРјР°С‚Сѓ .doc, .docx");   
 		}
 	}
 }
